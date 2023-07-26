393172b3d761dd2a35a06fdf5f651fb0415fade5
Allen <allenabraham@simplogics.com>
2023-07-26 12:04:04 +0530

    Made the classes different

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,105 @@
+import javax.swing.plaf.synth.SynthDesktopIconUI;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
-    public static Node addData(ArrayList<Integer> a){
-        Node l = new Node();
-        for(Integer i:a){
-            l.addNode(i);
+
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.getData() + " ");
+            temp = temp.getNext();
         }
-        return l;
     }
-    public static void loopCreation(Node l, int n){
-        Node t = l.getHead();
-        for(int i=0;i<=(n-1);i++){
+
+    public static Node addNode(int data, Node head, Node tail) {
+        Node n = new Node(data);
+//        System.out.println("Printing @ addNode ");
+//        System.out.println(head.getData()+" "+tail.getData());
+        if (head == null) {
+            head = n;
+
+        } else {
+            tail.setNext(n);
+        }
+//        System.out.println("@addNodeInnerBefore: " + head + " " + tail);
+
+        tail = n;
+//        System.out.println("Head's next" + head.getNext().getData());
+//        System.out.println("@addNodeInner: " + head + " " + tail);
+        return tail;
+//        System.out.println(head.getData()+" "+tail.getData());
+//        System.out.println("Done with the addNode");
+    }
+
+    public static Node addData(ArrayList<Integer> a, Node head, Node tail) {
+//        System.out.println("Starting Add Data");
+//        System.out.println("@addDataOuter: " + head + " " + tail);
+        for (int element : a) {
+//            System.out.println("for: " + element);
+//            System.out.println("Checking in the loop");
+//            System.out.println("Adding in "+element+" "+head.getData()+" "+tail.getData());
+            tail = addNode(element, head, tail);
+//            System.out.println("@addDataInner: " + head + " " + tail);
+//            System.out.println("@addData Printer:");
+//            System.out.println(head.getData()+" "+tail.getData());
+        }
+        return tail;
+//        System.out.println("Ending Add Data");
+    }
+
+
+    public static void loopCreation(int n, Node head, Node tail) {
+        Node t = head;
+        for (int i = 0; i <= (n - 1); i++) {
+            System.out.println("Checking the loop @: "+t.getData());
             t = t.getNext();
         }
-        Node temp = l.getTail();
+        System.out.println("Tail@ "+tail.getData());
+        Node temp = tail;
         temp.setNext(t);
-        //temp.next = l.head;
+//        To make it fully circular list.
+//        Node temp = tail;
+//        temp.setNext(head);
+//        return head;
     }
-    public static void checkLoop(Node l){
-        Node slowPointer = l.getHead();
-        Node fastPointer = l.getHead();
+
+    public static void checkLoop(Node head, Node tail) {
+        Node slowPointer = head;
+        Node fastPointer = head;
         int flag = 1;
-        while(slowPointer!=null && fastPointer!=null){
+        while (slowPointer != null && fastPointer != null) {
+            System.out.println(slowPointer.getData()+" "+fastPointer.getData());
             slowPointer = slowPointer.getNext();
             fastPointer = fastPointer.getNext().getNext();
-            if(slowPointer==fastPointer){
+            if (slowPointer == fastPointer) {
                 flag = 0;
                 System.out.println("Loop Exists");
                 break;
             }
         }
-        if(flag == 1){
+        if (flag == 1) {
             System.out.println("No Loop Exists");
         }
 
     }
+
     public static void main(String[] args) {
-        ArrayList<Integer> inputNumbers = new ArrayList<Integer>(Arrays.asList(3,6,9,12,15,18));
-        Node l = addData(inputNumbers);
-        loopCreation(l,4);
-        checkLoop(l);
+        Node l = new Node(1);
+        Node head = l;
+        Node tail = l;
+//        System.out.println("@main1: " + head + " " + tail);
+//        System.out.println("Printing the head data "+head.getData());
+//        System.out.println("Printing the tail data "+tail.getData());
+        ArrayList<Integer> inputNumbers = new ArrayList<Integer>(Arrays.asList(2, 3, 4, 5, 6, 7));
+        tail = addData(inputNumbers, head, tail);
+//        System.out.println("@main2: " + head + " " + tail);
+        printList(head);
+        System.out.println();
+//        System.out.println("Printing the head data "+head.getData());
+//        System.out.println("Printing the tail data "+tail.getData());
+        loopCreation(4, head, tail);
+//        printList(head);
+        checkLoop(head, tail);
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,8 +1,6 @@
 public class Node{
     private int data;
     private Node next;
-    private Node head;
-    private Node tail;
 
     public int getData(){
         return this.data;
@@ -10,15 +8,6 @@ public class Node{
     public Node getNext(){
         return this.next;
     }
-
-    public Node getHead(){
-        return this.head;
-    }
-
-    public Node getTail(){
-        return this.tail;
-    }
-
     public void setData(int data){
         this.data = data;
     }
@@ -26,38 +15,11 @@ public class Node{
     public void setNext(Node next){
         this.next = next;
     }
-
-    public void setHead(Node head){
-        this.head = head;
-    }
-
-    public void setTail(Node tail){
-        this.tail = tail;
-    }
-
     public Node(){
         this.next = null;
-        this.head = null;
-        this.tail = null;
     }
-
     public Node(int data) {
         this.data = data;
         this.next = null;
-        this.head = null;
-        this.tail = null;
-    }
-
-    public void addNode(int data){
-        Node n = new Node(data);
-        if(this.head==null){
-            this.head = n;
-
-        }else{
-            this.tail.next = n;
-        }
-        this.tail = n;
-        System.out.println("Added: "+n.data+" "+this.head.data+" "+this.tail.data);
     }
 }
-
